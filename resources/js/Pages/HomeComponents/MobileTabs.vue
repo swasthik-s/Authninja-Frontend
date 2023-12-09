17dc55664c2627ec967c5e48554cf42a1ddddc4e
Swasthik-s <136150261+swasthik-s@users.noreply.github.com>
2023-12-09 22:53:27 +0530

    Moved All Mobile View Tabs inside custom Vue File

---
+++ resources/js/Pages/HomeComponents/MobileTabs.vue
@@ -0,0 +1,9 @@
+<template>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/about">About</a>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/blog">Blog</a>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/customers">Customers</a>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/pricing">Pricing</a>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/changelog">Changelog</a>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/docs">Docs</a>
+  <a class="text-md block w-full border-b border-gray-500 py-4 font-semibold text-slate-100 transition duration-200 ease-in-out last:border-none hover:text-slate-400" href="/contact">Contact</a>
+</template>
