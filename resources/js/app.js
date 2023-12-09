5f7a37ca85263674b63abca5a2c5247a30e70693
Swasthik-s <136150261+swasthik-s@users.noreply.github.com>
2023-12-09 22:47:22 +0530

    Added tw-elements

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Animate, initTE } from "tw-elements";
+initTE({ Animate });
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
