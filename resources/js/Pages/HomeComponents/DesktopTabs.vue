f3d4d8f220ba8a57044abd006238f7b01b47c7cb
Swasthik-s <136150261+swasthik-s@users.noreply.github.com>
2023-12-09 22:49:29 +0530

    moved the tabs to new vue file

---
+++ resources/js/Pages/HomeComponents/DesktopTabs.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="flex items-center gap-4">
+    <li>
+      <a class="outline-none transition duration-150 ease-in-out rounded-md px-1 py-1 text-sm font-medium text-slate-100 hover:text-slate-400 lg:px-2" href="/about">About</a>
+    </li>
+    <li>
+      <a class="outline-none transition duration-150 ease-in-out rounded-md px-1 py-1 text-sm font-medium text-slate-100 hover:text-slate-400 lg:px-2" href="/blog">Blog</a>
+    </li>
+    <li>
+      <a class="outline-none transition duration-150 ease-in-out rounded-md px-1 py-1 text-sm font-medium text-slate-100 hover:text-slate-400 lg:px-2" href="/customers">Customers</a>
+    </li>
+    <li>
+      <a class="outline-none transition duration-150 ease-in-out rounded-md px-1 py-1 text-sm font-medium text-slate-100 hover:text-slate-400 lg:px-2" href="/pricing">Pricing</a>
+    </li>
+    <li>
+      <a class="outline-none transition duration-150 ease-in-out rounded-md px-1 py-1 text-sm font-medium text-slate-100 hover:text-slate-400 lg:px-2" href="/changelog">Changelog</a>
+    </li>
+    <li>
+      <a class="outline-none transition duration-150 ease-in-out rounded-md px-1 py-1 text-sm font-medium text-slate-100 hover:text-slate-400 lg:px-2" href="/docs">Docs</a>
+    </li>
+  </ul>
+</template>
